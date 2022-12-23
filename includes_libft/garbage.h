22a61006ffd74ecb8f95432a87c8474aa080ba17
Melodie SHAHRVARI <meshahrv@e3r10p23.clusters.42paris.fr>
2022-12-23 17:02:20 +0100

    DONE : quick fix garbage

--- includes_libft/garbage.h
+++ includes_libft/garbage.h
@@ -6,7 +6,7 @@
 /*   By: meshahrv <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/21 17:40:55 by meshahrv          #+#    #+#             */
-/*   Updated: 2022/12/21 17:49:49 by meshahrv         ###   ########.fr       */
+/*   Updated: 2022/12/23 16:58:39 by meshahrv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 # define GARBAGE_H
 
 # include "libft.h"
+
+void	*garbage_alloc(t_list **garb_list, int size);
 
 #endif
